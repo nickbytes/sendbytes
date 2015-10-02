6a827880aa8eb5f6df1299e5cbd0eedf2080553a
Nick Beattie <nick@nickbytes.com>
2015-10-01 22:47:11 -0400

    🆕 add promo section becuz marketing

--- app/components/backs/Rthanks.js
+++ app/components/backs/Rthanks.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Router from 'react-router'
 import base64 from 'js-base64'
 import Isvg from 'react-inlinesvg'
+import Promo from '../promo/Promo'
 
 var Rthanks = React.createClass({
   mixins: [Router.State],
@@ -24,21 +25,24 @@ var Rthanks = React.createClass({
       opacity: '0.5'
     }
     return(
-      <div className="m2 p2" style={outerCard}>
-        <div className="clearfix border p2 relative" style={innerCard}>
-          <div className="col-4 mx-auto" style={bug}>
-            <Isvg src="/assets/bug.svg"></Isvg>
-          </div>
-          <div className="sm-col md-col-10 lg-col-10">
-            <h2 className="h00-responsive center fancy maroon">Thanks for the {giftFinal}</h2>
-          </div>
+      <div>
+        <div className="m2 p2" style={outerCard}>
+          <div className="clearfix border p2 relative" style={innerCard}>
+            <div className="col-4 mx-auto" style={bug}>
+              <Isvg src="/assets/bug.svg"></Isvg>
+            </div>
+            <div className="sm-col">
+              <h2 className="h00-responsive center fancy maroon">Thanks for the {giftFinal}</h2>
+            </div>
 
-          <div className="absolute bottom-0 right-0 p2">
-            <h6 className="caps fancy maroon left-align">From</h6>
-            <h1 className="right-align m0">Your name</h1>
-          </div>
+            <div className="absolute bottom-0 right-0 p2">
+              <h6 className="caps fancy maroon left-align">From</h6>
+              <h1 className="right-align m0">Your name</h1>
+            </div>
 
+          </div>
         </div>
+        <Promo />
       </div>
     )
   }
---
+++ app/components/promo/Promo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Promo extends React.Component{
+  render(){
+    return(
+      <div>
+        <ul className="list-reset ">
+          <li className="gray h6">Cards from <a href="http://sendbytes.space">sendbytes.com</a></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Promo
